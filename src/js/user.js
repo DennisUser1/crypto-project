5ad86e1a291991a9a2a8778763c0251bd9dc41f1
Dennis Fedorov <115027699+DennisUser1@users.noreply.github.com>
2025-03-01 21:48:33 +0200

    v.1.7. Added files user.js

---
+++ src/js/user.js
@@ -0,0 +1,25 @@
+import { fetchUser } from './fakeApi.js';
+
+const domElements = {
+    headerUserId: document.querySelector('.header-user-id > span'),
+    userIcon: document.querySelector('.user-icon-wrap > img'),
+    menuUserId: document.querySelector('.menu-user-title > span'),
+    menuUserIcon: document.querySelector('.menu-user-icon-wrap > img'),
+    menuUserBalance: document.querySelector('.menu-user-balance'),
+};
+
+async function renderHeaderAndMenu() {
+    const userInfo = await fetchUser();
+    domElements.headerUserId.textContent = userInfo.id;
+    domElements.menuUserId.textContent = userInfo.id;
+    if (userInfo.photoUrl) {
+        domElements.userIcon.src = userInfo.photoUrl;
+        domElements.menuUserIcon.src = userInfo.photoUrl;
+        domElements.userIcon.classList.add('hasPicture');
+        domElements.menuUserIcon.classList.add('hasPicture');
+    }
+    const coins = userInfo.coins;
+    domElements.menuUserBalance.textContent = `${coins[0].value} ${coins[0].code} | ${coins[1].value} ${coins[1].code}`;
+}
+
+renderHeaderAndMenu();
